d70fcb1e612a9639f34c9754781466fa3f933c9c
jordan <jordan.lesich@gmail.com>
2023-08-17 10:18:37 -0700

    test email API route webhooks message recieve

--- apps/web/src/pages/api/webhook/email.ts
+++ apps/web/src/pages/api/webhook/email.ts
@@ -4,7 +4,7 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     // Handle email webhook payload here
     // ...
-    console.log('')
+    console.log('req.body', req.body)
     res.status(200).send({ status: 'Email webhook received' })
   } else {
     res.status(405).send({ error: 'Method Not Allowed' })
